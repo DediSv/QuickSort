62bf6a6f8e0a50e16ccfa06fc24c2732f8910a18
DediSv <165560773+DediSv@users.noreply.github.com>
2024-04-01 22:51:05 +0200

    Deleted the first variant of the sort function

--- main.cpp
+++ main.cpp
@@ -19,26 +19,6 @@ void Vypis(vector <int> array){
     cout << "\n";
 }
 
-/*void QuickSort(vector <int> &array, int a, int n){ //a - begin, n - num of elements
-    ///Partition the array(<p p >p)///
-    if (a < n) { //Jeli array má vic než 1 element
-        int left = a; //pointer on the start
-        int right = n; // on the end
-        int pivotIndex = rand() % (n - a + 1) + a; //vyber pivota
-        int pivot = array[pivotIndex]; //remember the meaning of pivot
-        swap(array[pivotIndex], array[right]); //swap pivot and the end
-        for (int i = left; i < n; i++) {
-            if (array[i] < pivot) {
-                swap(array[i], array[a]);
-                a++;
-            }
-        }
-        swap(array[right], array[a]); //putting the pivot on his place
-        QuickSort(array, left, a - 1);
-        QuickSort(array, a + 1, right);
-    }
-}*/
-
 void QuickSort(vector<int> &array, int a, int n){
     int p, i, j; //pivot, pointer leftSide, pointer rightSide
     if (a < n){
